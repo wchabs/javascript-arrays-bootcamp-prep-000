e3a9463cabaf435c020bbb9cb5260fba3d2c08f9
William Chabla <w_chabla@hotmail.com>
2018-03-07 17:05:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,28 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array, element) {
+  var newArray = [element, ...array];
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element);
+}
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
